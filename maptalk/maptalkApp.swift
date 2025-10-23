2c78e3b2474cfdf3c24ed2da9246df6e0d5bb1ed
Chengsi <soap27century@163.com>
2025-10-22 22:47:44 -0700

    Initial Commit

---
+++ maptalk/maptalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  maptalkApp.swift
+//  maptalk
+//
+//  Created by chengsi yang on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct maptalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maptalkTests/maptalkTests.swift
@@ -0,0 +1,16 @@
+//
+//  maptalkTests.swift
+//  maptalkTests
+//
+//  Created by chengsi yang on 10/22/25.
+//
+
+import Testing
+
+struct maptalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
